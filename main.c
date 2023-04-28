39d6b557422db8693e82cd04e82f8718d4bc2c21
Gerald <gerryoncode@gmail.com>
2023-04-28 01:29:06 -0700

    main.c changed a little

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
 #include "main.h"
 /**
- * main - initialize the variables of the program
- * @argc: number of values received from the command line
- * @argv: values received from the command line
- * @env: number of values received from the command line
- * Return: zero on succes.
+ * main - initialize variables
+ * @argc: no. of values from command line
+ * @argv: values from the command line
+ * @env: no. of values received from command line
+ * Return:0 when successful
  */
 int main(int argc, char *argv[], char *env[])
 {
-	data_of_program data_struct = {NULL}, *data = &data_struct;
-	char *prompt = "";
+	data_of_program data_struct = {NULL}, *dataVal = &data_struct;
+	char *promptStr = "";
 
-	inicialize_data(data, argc, argv, env);
+	inicialize_data(dataVal, argc, argv, env);
 
 	signal(SIGINT, handle_ctrl_c);
 
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
-	{/* We are in the terminal, interactive mode */
-		errno = 2;/*???????*/
-		prompt = PROMPT_MSG;
+	{
+		errno = 2;
+		promptStr = PROMPT_MSG;
 	}
 	errno = 0;
-	sisifo(prompt, data);
+	sisifo(promptStr, dataVal);
 	return (0);
 }
 
 /**
- * handle_ctrl_c - print the prompt in a new line
- * when the signal SIGINT (ctrl + c) is send to the program
+ * handle_ctrl_c - print new line prompt SIGINT (ctrl + c) is pressed
  * @UNUSED: option of the prototype
  */
 void handle_ctrl_c(int opr UNUSED)
@@ -37,11 +36,11 @@ void handle_ctrl_c(int opr UNUSED)
 }
 
 /**
- * inicialize_data - inicialize the struct with the info of the program
- * @data: pointer to the structure of data
- * @argv: array of arguments pased to the program execution
- * @env: environ pased to the program execution
- * @argc: number of values received from the command line
+ * inicialize_data - init the struct with the program info
+ * @data: pointer
+ * @argv: array of arg passed to program
+ * @env: environ passed to the program
+ * @argc: no of values received from the command line
  */
 void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
@@ -51,7 +50,7 @@ void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 	data->input_line = NULL;
 	data->command_name = NULL;
 	data->exec_counter = 0;
-	/* define the file descriptor to be readed*/
+
 	if (argc == 1)
 		data->file_descriptor = STDIN_FILENO;
 	else
@@ -87,7 +86,7 @@ void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 /**
  * sisifo - its a infinite loop that shows the prompt
  * @prompt: prompt to be printed
- * @data: its a infinite loop that shows the prompt
+ * @data: infinite loop to display prompt
  */
 void sisifo(char *prompt, data_of_program *data)
 {
@@ -101,7 +100,7 @@ void sisifo(char *prompt, data_of_program *data)
 		if (error_code == EOF)
 		{
 			free_all_data(data);
-			exit(errno); /* if EOF is the fisrt Char of string, exit*/
+			exit(errno);
 		}
 		if (string_len >= 1)
 		{
@@ -109,7 +108,7 @@ void sisifo(char *prompt, data_of_program *data)
 			expand_variables(data);
 			tokenize(data);
 			if (data->tokens[0])
-			{ /* if a text is given to prompt, execute */
+			{
 				error_code = execute(data);
 				if (error_code != 0)
 					_print_error(error_code, data);
